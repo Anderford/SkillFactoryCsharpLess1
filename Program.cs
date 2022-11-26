21edab49703546342702340f2036949737fcc55b
Anderford <incoqwert@gmail.com>
2022-11-26 17:54:59 +0300

    Task 3.7.4

--- Program.cs
+++ Program.cs
@@ -17,6 +17,15 @@ class MainClass
         Console.WriteLine("My shoe size is " + MyShoeSize);
         Console.WriteLine(DayOfWeek.Sunday);
         Console.WriteLine(result);
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.Write("Enter your age: ");
+        byte age = (byte)int.Parse(Console.ReadLine());
+        Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+        Console.WriteLine("What is your favorite day of week?");
+        DayOfWeek day = (DayOfWeek) int.Parse(Console.ReadLine());
+        Console.WriteLine($"You favotite day is {day}");
+        Console.ReadKey();
     }
 
 
